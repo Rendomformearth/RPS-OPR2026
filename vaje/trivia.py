ee71bc4d88837f031157109ed27762fa0e611980
Rendomformearth <160880867+Rendomformearth@users.noreply.github.com>
2026-09-24 05:58:02 +0000

    gg

--- vaje/trivia.py
+++ vaje/trivia.py
@@ -1,12 +1,18 @@
 import requests
-import pprint from pprint
 import random
 import html
 vpr = int(input("Koliko vprašanj želiš? "))
 url = f"https://opentdb.com/api.php?amount={vpr}"
 klic = requests.get(url).json()
-vprasanje = klic["result"]
-score  = 0
-for v in vprasanje():
-    prav = v["correct_answer"]
-    odgovori = v["incorrect_answer"] + [prav]
+vprasanja = klic["results"]
+
+for vprasanje in  vprasanja:
+    odgovori = vprasanje["incorrect_answers"]
+    odgovori.append(vprasanje["correct_answer"])
+    random.shuffle(odgovori)
+    print(vprasanje["question"])
+    for i, odgovor in enumerate(odgovori):
+        print(i+1, odgovor)
+    odgovor=int(input("odgovor: "))
+    if odgovori[odgovor-1] == vprasanje["correct_answer"]:
+        print("correct")
